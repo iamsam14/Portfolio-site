dfe17531024fb2da686dead17c0128ded161d975
iamsam14 <sam.robles.199@gmail.com>
2020-10-30 14:13:35 -0400

    navbar

--- client/src/Components/NavigationBar.jsx
+++ client/src/Components/NavigationBar.jsx
@@ -3,7 +3,7 @@ import { Nav, Navbar } from 'react-bootstrap';
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="light" variant=""  fixed="top">
+    <Navbar bg="light" variant="" fixed="top">
       <Nav className="small">
         <Nav.Link href="https://github.com/iamsam14">GitHub</Nav.Link>
         <Nav.Link href="https://www.linkedin.com/in/chefsrobles">
