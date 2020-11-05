b4908eff5de97f2176534d1788af00fa9679d2cf
iamsam14 <sam.robles.199@gmail.com>
2020-11-05 18:16:18 -0500

    sass compiler

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './Pages/Home'
-import "./App.scss";
+import Home from './Pages/Home';
+import './dist.css/App.css';
 
 function App() {
   return (
-  <div className="main-container">
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' component={Home} />
-      </Switch>
-    </BrowserRouter>
-  </div>
+    <div className="main-container">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- client/src/Components/Layers/ProjectCards.jsx
+++ client/src/Components/Layers/ProjectCards.jsx
@@ -4,20 +4,24 @@ import trivia from '../images/trivia.png';
 
 const ProjectCards = () => {
   return (
-    <div className="container">
+    <div className="projectscontainer">
       <div className="projectcard">
-        <a href="https://cuenta-app-final.herokuapp.com/">
-          <img src={Cuenta} className="projectimage" alt="projectimg" />
-        </a>
-        <h5>Cuenta</h5>
-        <p>Heroku | Stripe | QR Scanner | MERN</p>
+        <div className="flexcard">
+          <a href="https://cuenta-app-final.herokuapp.com/">
+            <img src={Cuenta} className="projectimage" alt="projectimg" />
+          </a>
+          <h5>Cuenta</h5>
+          <p>Heroku | Stripe | QR Scanner | MERN</p>
+        </div>
       </div>
       <div className="projectcard">
-        <a href="https://infinite-fortress-16227.herokuapp.com/">
-          <img src={trivia} className="projectimage" alt="projectimg" />
-        </a>
-        <h5>Trivia Game</h5>
-        <p>Axios | RESTful API | React | Javascript</p>
+        <div className="flexcard">
+          <a href="https://infinite-fortress-16227.herokuapp.com/">
+            <img src={trivia} className="projectimage" alt="projectimg" />
+          </a>
+          <h5>Trivia Game</h5>
+          <p>Axios | RESTful API | React | Javascript</p>
+        </div>
       </div>
     </div>
   );
