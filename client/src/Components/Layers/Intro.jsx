0238dd2ae49746c8526c68bf6ee7069ad2a3d96b
iamsam14 <sam.robles.199@gmail.com>
2020-11-13 19:07:35 -0500

    water-wave removed

--- client/src/Components/Layers/Intro.jsx
+++ client/src/Components/Layers/Intro.jsx
@@ -1,32 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import WaterWave from 'react-water-wave';
+import { render } from 'react-dom';
 import NavigationBar from '../NavigationBar';
 
 const Intro = () => {
-  const [compHeight, setCompHeight] = useState('675px');
-  useEffect(() => {
-    window.innerWidth >= 1299
-      ? setCompHeight('675px')
-      : window.innerWidth < 1299 && window.innerWidth >= 1019
-      ? setCompHeight('800px')
-      : window.innerWidth < 1019 && window.innerWidth >= 674
-      ? setCompHeight('879px')
-      : window.innerWidth < 674 && window.innerWidth >= 652
-      ? setCompHeight('921px')
-      : window.innerWidth < 652 && window.innerWidth >= 537
-      ? setCompHeight('1041px')
-      : window.innerWidth < 537 && window.innerWidth >= 526
-      ? setCompHeight('1162px')
-      : window.innerWidth < 526
-      ? setCompHeight('1244px')
-      : setCompHeight('675px');
-  }, [window.innerWidth]);
   return (
     <div className="water-color">
       <NavigationBar className="color" />
-      <WaterWave
-        style={{ minHeight: compHeight, width: '100%', position: 'absolute' }}
-      />
       <div className="intro color">
         <div className="color" id="waterCanvas">
           <h3>Hello World, I'm Samuel!</h3>
