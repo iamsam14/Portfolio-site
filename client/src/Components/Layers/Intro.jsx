de1c6e5cd41eb9084662a836cbba3c8260168bc5
iamsam14 <sam.robles.199@gmail.com>
2021-01-21 22:23:41 -0500

    clean up

--- client/src/Components/Layers/Intro.jsx
+++ client/src/Components/Layers/Intro.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
 import NavigationBar from '../NavigationBar';
 
 const Intro = () => {
